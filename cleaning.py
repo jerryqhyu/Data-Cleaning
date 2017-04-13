2894b554037280740530be3265e9a5ab6c2257ea
MenghanC <menghan.chen18@gmail.com>
2017-04-12 21:03:03 -0400

    gg

--- cleaning.py
+++ cleaning.py
@@ -10,13 +10,13 @@ from util import data_reader
 install_aliases()
 
 
-class Logistic_Cleaner():
+class Cleaner():
 
     def __init__(self, theta):
         reader = data_reader()
         # saver = image_saver()
         n, a, b, c, d = reader.load_mnist()
-        #
+
         self.train_data = np.round(a)[:10000]
         self.valid_data = np.round(c)[:5000]
         self.train_labels = b[:10000]
@@ -25,17 +25,37 @@ class Logistic_Cleaner():
         self.w = np.zeros((self.train_labels.shape[1], self.train_data.shape[1]))
         self.theta = theta
 
-    def logistic_likelihood(self):
-        log_likelihood = logistic_ll(self.train_data, self.w)
+    def logistic_likelihood(self, x, w):
+        log_likelihood = logistic_ll(x, w)
         return np.exp(log_likelihood)
 
-    def weighted_likelihood(self, likelihood):
-        return np.dot(self.theta, likelihood.T)
+    def weighted_likelihood(self, x, y, w, theta, likelihood_fcn):
+        likelihood = likelihood_fcn(x, w)
+        return np.sum(np.multiply(y, np.log(np.dot(theta, likelihood.T)).T))
 
-    def train_logistic(self, lambda=0.01, epoch=500):
-        likelihood = self.weighted_likelihood(self.logistic_likelihood())
-        for i in epoch:
-            pass
+    def train_logistic(self, learning_rate=0.01, epoch=500):
+        gradient = grad(self.weighted_likelihood, argnum=2)
+        for i in range(epoch):
+            self.w += gradient(self.train_data, self.train_labels, self.w, self.theta, self.logistic_likelihood)
+        return self.w
+
+    def metrics(self):
+        # likelihood
+        ll = np.multiply(self.train_labels, pred_ll(self.train_data, self.w))
+        test_ll = np.multiply(self.valid_labels, pred_ll(self.valid_data, self.w))
+
+        # accuracy
+        pred = pred_ll(self.train_data, self.w)
+        test_pred = pred_ll(self.valid_data, self.w)
+        print("average predictive log ll for training set is:")
+        print(np.sum(ll)/30)
+        print("average predictive log ll for test set is:")
+        print(np.sum(test_ll)/10000)
+
+        print("average predictive accuracy for training set is:")
+        print(predictive_accuracy(pred, self.train_labels))
+        print("average predictive accuracy for test set is:")
+        print(predictive_accuracy(test_pred, self.valid_labels))
 
 
 def logistic_ll(x, w):
@@ -60,37 +80,11 @@ def logll(K):
     return K - np.tile(logsumexp(K, axis=1), 10).reshape(10, K.shape[0]).T
 
 
-def logistic_metrics(images, labels, t_images, t_labels):
-    train_x = images[:30]
-    train_y = labels[:30]
-    test_x = t_images[:10000]
-    test_y = t_labels[:10000]
-    w = fit_logistic(train_x, train_y)
-
-    # likelihood
-    ll = np.multiply(train_y, pred_ll(train_x, w))
-    test_ll = np.multiply(test_y, pred_ll(test_x, w))
-
-    # accuracy
-    pred = pred_ll(train_x, w)
-    test_pred = pred_ll(test_x, w)
-    print("average predictive log ll for training set is:")
-    print(np.sum(ll)/30)
-    print("average predictive log ll for test set is:")
-    print(np.sum(test_ll)/10000)
-
-    print("average predictive accuracy for training set is:")
-    print(predictive_accuracy(pred, train_y))
-    print("average predictive accuracy for test set is:")
-    print(predictive_accuracy(test_pred, test_y))
-
-    return w
-
-
 def predictive_accuracy(proposed, true):
     return np.sum(np.equal(np.argmax(proposed, axis=1), np.argmax(true, axis=1)))/true.shape[0] * 100
 
 
 if __name__ == '__main__':
-    cleaner = Logistic_Cleaner(0)
-    cleaner.logistic_likelihood()
+    cleaner = Cleaner(0)
+    cleaner.train_logistic()
+    cleaner.metrics()
